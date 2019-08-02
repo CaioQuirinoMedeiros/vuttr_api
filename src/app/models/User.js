f91249aa1868c995e701de47f3747b0f58611dba
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-02 04:48:41 -0300

    Changing minimum password length to 6

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      minlength: 7,
+      minlength: 6,
       trim: true
     },
     tokens: [
